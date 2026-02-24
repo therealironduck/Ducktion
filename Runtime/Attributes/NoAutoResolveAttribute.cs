333fa6c30a90648560e23921b5e35149f0bc1dc5
Jordan Kniest <mail@jkniest.de>
2026-02-24 20:41:51 +0100

    Update Runtime/Attributes/NoAutoResolveAttribute.cs
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Runtime/Attributes/NoAutoResolveAttribute.cs
+++ Runtime/Attributes/NoAutoResolveAttribute.cs
@@ -4,7 +4,7 @@ namespace TheRealIronDuck.Ducktion.Attributes
 {
     /// <summary>
     /// This attribute will prevent any service from being automatically resolved.
-    /// Meaning the service needs to registered explicitly. This can be helpful if the service
+    /// Meaning the service needs to be registered explicitly. This can be helpful if the service
     /// has parameters that cannot be resolved smartly.
     ///
     /// As an example within the core, see the "TaggedServices" class.
